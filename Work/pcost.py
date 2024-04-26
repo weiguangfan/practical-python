6e8deba3462ca2a96cf965b27e97a77c61359b05
weiguangfan <826008728@qq.com>
2024-04-26 17:42:59 +0800

    增加了一些测试文件，跟随着教程

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
