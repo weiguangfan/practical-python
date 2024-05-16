8f416b98ea059ec7fb165df6790eff5184feea04
weiguangfan <826008728@qq.com>
2024-05-16 18:30:37 +0800

    03-03

---
+++ Work/016.py
@@ -0,0 +1,35 @@
+def add(x, y):
+    return x + y
+
+
+print(add(3, 4))
+print(add('Hello', 'World'))
+print(add('3', '4'))
+
+# print(add(3, '4'))  # 报错
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
